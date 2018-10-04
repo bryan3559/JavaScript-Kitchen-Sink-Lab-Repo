6aea3ee1554dbbc39a23cc58df6688c2ed020e17
Bryan Clayton <bclayton2k8@aol.com>
2018-10-03 20:26:06 -0500

    Created HTML file and JavaScript file. Linked both files and added a function, contant, and variable to JavaScript file. Also added project details to README file.

---
+++ app.js
@@ -0,0 +1,14 @@
+let fullName = 'Bryan Clayton';
+
+console.log (fullName);
+
+const totalStates = 50;
+
+console.log (totalStates);
+
+let sum = add(5 , 4);
+console.log (sum);
+
+function add(num1 , num2) {
+    return num1 + num2;
+}
